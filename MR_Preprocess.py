92a78228ff9376fd51fe171b33486c136a0dd947
a6225301 <412827216@qq.com>
2018-10-19 14:15:52 +0800

    add read log file

--- MR_Preprocess.py
+++ MR_Preprocess.py
@@ -7,18 +7,27 @@ import pydicom
 from glob import glob
 
 #standard data
-case_path='C:\\Users\\HeHe\\Desktop\\dcm\\BAO_XUN_YIN_100312588\\PELVIS_PROSTATE_PROGRAM_20111121_101613_906000\\T2_TSE_TRA_FS_0005'
+case_path='C:\\Users\\ilab\\Desktop\\dcm\\BAO_XUN_YIN_100312588\\PELVIS_PROSTATE_PROGRAM_20111121_101613_906000\\T2_TSE_TRA_FS_0005'
+case_path='C:\\Users\\ilab\\Desktop\\dcm\\BAO_XUN_YIN_100312588\\PELVIS_PROSTATE_PROGRAM_20111121_101613_906000\\b-1500'
 
 
-case_path='C:\\Users\\HeHe\\Desktop\\dcm\\xian-ai\\HUYONGSHENG_50172302\\PELVIS_PROSTATE_PROGRAM_20150129_135211_921000\\T2_TSE_TRA_FS_0003'
+# case_path='C:\\Users\\ilab\\Desktop\\dcm\\xian-ai\\HUYONGSHENG_50172302\\PELVIS_PROSTATE_PROGRAM_20150129_135211_921000\\T2_TSE_TRA_FS_0003'
+# case_path='C:\\Users\\ilab\\Desktop\\dcm\\xian-ai\\HUYONGSHENG_50172302\\PELVIS_PROSTATE_PROGRAM_20150129_135211_921000\\b-1500'
 
 
-# case_path='C:\\Users\\HeHe\\Desktop\\dcm\\xian-ai\\LIU_HANG_GONG_100371672\\PELVIS_PROSTATE_PROGRAM_20120502_115455_734000\\T2_TSE_TRA_FS_0003'
-case_path_mhd='C:\\Users\\HeHe\\Desktop\\dcm\\xian-ai\\aaa.mhd'
+# case_path='C:\\Users\\ilab\\Desktop\\dcm\\xian-ai\\ZHUQINGQUAN_100665572\\PELVIS_PROSTATE_PROGRAM_20140509_084125_546000\\T2_TSE_TRA_FS_0002'
+
+# case_path='C:\\Users\\ilab\\Desktop\\dcm\\xian-ai\\LIU_HANG_GONG_100371672\\PELVIS_PROSTATE_PROGRAM_20120502_115455_734000\\T2_TSE_TRA_FS_0003'
+# case_path='C:\\Users\\ilab\\Desktop\\dcm\\xian-ai\\HUYONGSHENG_50172302\PELVIS_PROSTATE_PROGRAM_20150129_135211_921000\\T2_TSE_TRA_FS_0003'
 
 
-nodule_coord=(-16.89,-38.82,-22.52)
+case_path_mhd='C:\\Users\\ilab\\Desktop\\dcm\\xian-ai\\aaa.mhd'
 
+
+nodule_coord=(-29.79,-59.49,-66.97)
+
+nodule_coord=(4.10,-54.13,36.89)
+# -19.42,-100.33,39.10
 
 
 nodule_coord=(nodule_coord[0]*(-1),nodule_coord[1]*(-1),nodule_coord[2])
@@ -43,12 +52,18 @@ def filter_dicom_names(case_path):
         if series_id <> dicom_index or i==0:
             dicom_index=series_id
             dicom_names.append(img_files_path[i])
-
+    print (dicom_names)
     dicom_names=tuple(dicom_names)
     
     return dicom_names
 
     
+def write_log(file_names):
+    with open('log.txt','a+') as f:
+        f.writelines('==========================\n')
+        f.writelines(file_names)
+        f.writelines('\n==========================\n')
+
 
 def resample(image, original_spacing, new_spacing=[4.5,4.5,4.5],order=3):
     # Determine current pixel spacing
@@ -63,14 +78,19 @@ def resample(image, original_spacing, new_spacing=[4.5,4.5,4.5],order=3):
 
 def plot_series_nodule(nodule_coord):
     reader = sitk.ImageSeriesReader()
-    dicom_names = reader.GetGDCMSeriesFileNames(case_path)
+    series_ids = reader.GetGDCMSeriesIDs(case_path)
+    # print ('*****************************')
+    # print (series_ids)
+
+    dicom_names = reader.GetGDCMSeriesFileNames(case_path,series_ids[0])
 
     dicom_names=filter_dicom_names(case_path)
 
     print ('==================')
     print (dicom_names)
-    print ('=====================')
+    write_log(dicom_names)
     print (len(dicom_names))
+    print ('=====================')
     reader.SetFileNames(dicom_names)
     image = reader.Execute()
     # print (image)
@@ -79,7 +99,7 @@ def plot_series_nodule(nodule_coord):
     spacing_xyz =np.array(image.GetSpacing())  # x, y, z
     print (origin_xyz,spacing_xyz)
     print (img_array.shape)
-    sitk.WriteImage(image, 'abc.mhd') 
+    # sitk.WriteImage(image, 'abc.mhd') 
 
 
     for i in range(img_array.shape[0]):
@@ -125,7 +145,7 @@ def plot_mhd_nodule(nodule_coord):
     center_xyz = (nodule_coord[0], nodule_coord[1], nodule_coord[2])
 
     index_coord=image.TransformPhysicalPointToIndex (center_xyz)
-    print (index_coord)
+    # print (index_coord)
 
     # index_coord=(320,262,3)
 
